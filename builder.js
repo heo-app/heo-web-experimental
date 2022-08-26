ce27c3ab4a380c12d9b9b106e8f8655bb0b9c936
mersad <mersad_998@yahoo.com>
2022-08-26 16:25:32 +0430

    chore : change execute port to 3000

--- builder.js
+++ builder.js
@@ -10,7 +10,7 @@ const isDev = process.env.NODE_ENV !== 'production'
  * @link https://www.npmjs.com/package/live-server#usage-from-node
  */
 const serverParams = {
-  port: 8181, // Set the server port. Defaults to 8080.
+  port: 3000, // Set the server port. Defaults to 8080.
   root: 'dist', // Set root directory that's being served. Defaults to cwd.
   open: true // When false, it won't load your browser by default.
   // host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
@@ -37,8 +37,9 @@ const buildParams = {
   sourcemap: true,
   logLevel: 'error',
   incremental: true
-}
-;(async () => {
+};
+
+(async () => {
   fs.removeSync('dist')
   fs.copySync('public', 'dist')
 
