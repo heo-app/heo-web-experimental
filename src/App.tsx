640ba55e30dfbb7db60dc6d8e63be283f168b8b1
mersad <mersad_998@yahoo.com>
2022-08-31 19:19:32 +0430

    feat: wrap theme provider on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ReactElement } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/system';
 
 import { customRoutes } from './chore/routes';
+import { customTheme } from './chore/themeProvider';
 
 export default function App(): ReactElement {
-  console.log('1');
-
-  console.log('2');
-  return <BrowserRouter>{customRoutes}</BrowserRouter>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <BrowserRouter>{customRoutes}</BrowserRouter>
+    </ThemeProvider>
+  );
 }
