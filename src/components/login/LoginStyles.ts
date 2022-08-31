fd40859a97732c83006705234357b99d891d78c6
mersad <mersad_998@yahoo.com>
2022-08-31 19:21:37 +0430

    feat : handle styles

---
+++ src/components/login/LoginStyles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles(() => ({
+  content: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
--- src/components/login/LoginView.tsx
+++ src/components/login/LoginView.tsx
@@ -1,7 +1,12 @@
 import { ReactElement } from 'react';
+import { TextField } from '@mui/material';
+
+import { useStyles } from './LoginStyles';
 
 const LoginView = (): ReactElement => {
-  return <div>login please</div>;
+  const classes = useStyles();
+
+  return <div className={classes.content}>login please</div>;
 };
 
 export default LoginView;
